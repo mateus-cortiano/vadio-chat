5ee944a66deacfb19eaa93953569447d73c45bde
mateus schwarz <mateus.cortiano@gmail.com>
2022-05-20 19:35:35 -0300

    -

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const jsonfile = require('../package.json')
 
+/* updates production package.json */
+
 jsonfile.scripts = { start: 'node ./server/main.js' }
 jsonfile.devDependencies = {}
 
 fs.writeFileSync('./dist/package.json', JSON.stringify(jsonfile))
-
-console.log(jsonfile)
