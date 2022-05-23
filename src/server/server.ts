e274967b4eac5ce41e04b01b339d69e0b3dba58e
mateus schwarz <mateus.cortiano@gmail.com>
2022-05-23 18:09:26 -0300

    - formatting

--- src/server/server.ts
+++ src/server/server.ts
@@ -18,9 +18,9 @@ import {
 // ---
 
 export class Server {
-  public server: http.Server
+  public http: http.Server
   public app: express.Express
-  public io: socketio.Server<
+  public socket: socketio.Server<
     ClientToServerEvents,
     ServerToClientEvents,
     InterServerEvents,
@@ -30,8 +30,8 @@ export class Server {
 
   constructor(public port: number, public public_path: string = '../client') {
     this.app = express()
-    this.server = http.createServer(this.app)
-    this.io = new socketio.Server(this.server)
+    this.http = http.createServer(this.app)
+    this.socket = new socketio.Server(this.http)
     this.logger = Logger.get_logger(this.constructor.name)
 
     this.app
@@ -54,15 +54,15 @@ export class Server {
 
   public emit_message(message: SocketData) {
     this.logger.info(` > ${message.author}: ${message.content}`)
-    this.io.emit('server_message', message)
+    this.socket.emit('server_message', message)
   }
 
   public on_connection(callback: (socket: socketio.Socket) => void) {
-    this.io.on('connection', callback)
+    this.socket.on('connection', callback)
   }
 
   public start() {
     this.logger.info(` * server starting @ port ${this.port}`)
-    this.server.listen(this.port)
+    this.http.listen(this.port)
   }
 }
